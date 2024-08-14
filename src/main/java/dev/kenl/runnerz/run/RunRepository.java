5ecb08261fd3ff1017568df2e2b53d9d6ec64e38
Ken329 <ken_037729@hotmail.com>
2024-08-14 16:28:30 +0800

    [feat] in-memory database setup and repository setup

--- src/main/java/dev/kenl/runnerz/run/RunRepository.java
+++ src/main/java/dev/kenl/runnerz/run/RunRepository.java
@@ -1,16 +1,11 @@
 package dev.kenl.runnerz.run;
 
-import dev.kenl.runnerz.Application;
-import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
